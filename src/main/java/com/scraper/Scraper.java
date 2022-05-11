cd1abce09184db0f12d823d054bf38a2fa6d2114
SSaiDai <spiderz1402@gmail.com>
2022-05-11 17:46:36 +0200

    Fixed nameXPath

--- src/main/java/com/scraper/Scraper.java
+++ src/main/java/com/scraper/Scraper.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Scraper {
     private ArrayList<Person> emperors;
     private final String url = "https://it.wikipedia.org/wiki/Imperatori_romani";
-    private final String nameXPath = "//*[@id=\"mw-content-text\"]/div[1]/table[2]/tbody/tr[1]/th[2]";
+    private final String nameXPath = "//*[@id=\"mw-content-text\"]/div[1]/table[2]/tbody/tr/td[2]";
     private final WebDriver driver = new HtmlUnitDriver();
 
     public Scraper(){
@@ -24,6 +24,10 @@ public class Scraper {
 
         List<WebElement> names = driver.findElements(By.xpath(nameXPath));
 
-        System.out.println(names);
+        for(WebElement name : names){
+            System.out.println(name.getText());
+        }
+
+        driver.close();
     }
 }
