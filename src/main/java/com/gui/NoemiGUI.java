d1d42bd80901ad3ae4a53f113550a5205d2f0e12
guizzyy <dieguz1507@gmail.com>
2022-05-18 16:45:38 +0200

    Gui development

--- src/main/java/com/gui/NoemiGUI.java
+++ src/main/java/com/gui/NoemiGUI.java
@@ -1,8 +1,23 @@
 package com.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class NoemiGUI {
-    private JPanel panel1;
-    private JButton OK;
+public class NoemiGUI extends JFrame{
+    public static void main(String[] args) {
+        NoemiGUI gui = new NoemiGUI();
+    }
+    public NoemiGUI(){
+        super("Imperatori Romani");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setExtendedState(MAXIMIZED_BOTH);
+        JLabel label = new JLabel("Scegli la dinastia romana:");
+        JComboBox menù = new JComboBox();   //Va aggiunta la lista delle dinastie
+        Container cont = this.getContentPane();
+        cont.setLayout(new BorderLayout());
+        cont.add(menù, BorderLayout.CENTER);
+        cont.add(label, BorderLayout.NORTH);
+        this.setVisible(true);
+    }
+
 }
