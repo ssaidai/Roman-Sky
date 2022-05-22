139625b7398840aefc64fae66f8992dac47d94c9
Victor Lopata <victorlopata2002@gmail.com>
2022-05-22 23:02:48 +0200

    Creazione di MyFont.java, aggiunta di font minimal e implementazione dei metodi ActionPerormed e setupListener. Rimozione del GridLayout su pCentro2.

---
+++ src/main/java/com/gui/MyFont.java
@@ -0,0 +1,30 @@
+package com.gui;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+
+// FONT SCARICABILI DAL SITO https://www.1001fonts.com/minimalistic-fonts.html?page=1 O DA UNO QUALSIASI
+// BASTA CHE SIA .TTF
+public class MyFont {
+
+
+    public static Font creaFont(String path, float size) {
+        Font customFont = null;
+        try {
+            //create the font to use. Specify the size!
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File(path)).deriveFont(size);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            //register the font
+            ge.registerFont(customFont);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        }
+
+        return customFont;
+    }
+
+}
--- src/main/java/com/gui/Window.java
+++ src/main/java/com/gui/Window.java
@@ -2,11 +2,16 @@ package com.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.IOException;
 
 
 //  TODO: RINOMINATE LE VARIABILI IN INGLESE, ETTA HA DETTO CHE IL CODICE DEVE ESSERE IN INGLESE .-.
 
-public class Window extends JFrame{
+public class Window extends JFrame implements ActionListener{
 
     private Container cont;
     private JLabel logoPrincipale =new JLabel();
@@ -20,105 +25,98 @@ public class Window extends JFrame{
     private JPanel pCentro2CENTRO = new JPanel();
     private JPanel pCentro2SUD = new JPanel();
     private JTextField testoRoma = new JTextField("Questa è Roma!");
-    private JLabel testoTendina = new JLabel("SCEGLI LA DINASTIA: ");
+    private JLabel testoTendina = new JLabel("SCEGLI LA DINASTIA ");
     private JLabel labSx = new JLabel();
     private JLabel labDx = new JLabel();
-    private JLabel testoProvvisiorio = new JLabel();
-    private JLabel testoProvvisiorio2 = new JLabel();
+    private JLabel immagineSfondo = new JLabel();
+    private JLabel immagineSfondo2 = new JLabel();
     private JLabel labLogo = new JLabel();
     private JLabel labSPQR = new JLabel();
     private JLabel spazioBianco = new JLabel();
-
-
-
-    private String[] dynasties = {"               DINASTIA GIULIO CLAUDIA               ", "               GUERRA CIVILE ROMANA               ", "               DINASTIA DEI FLAVI               ", "               IMPERATORI ADOTTIVI               ", "               GUERRA CIVILE ROMANA2               ", "               DINASTIA DEI SEVERI               ", "               ANARCHIA MILITARE               ", "               DINASTIA VALERIANA               ", "               IMPERATORI ILLIRICI               ", "               RIFORMA TETRARCHICA               ", "               GUERRA CIVILE ROMANA_3               ", "               DINASTIA COSTANTINIANA               ", "               CASATA VALENTINIANO TEODOSIO               ", "               CASATA TEODOSIO               ", "               ULTIMI IMPERATORI               "};
+    private String[] dynasties = {"DINASTIA GIULIO CLAUDIA", "GUERRA CIVILE ROMANA", "DINASTIA DEI FLAVI", "IMPERATORI ADOTTIVI", "GUERRA CIVILE ROMANA 2", "DINASTIA DEI SEVERI", "ANARCHIA MILITARE", "DINASTIA VALERIANA", "IMPERATORI ILLIRICI", "RIFORMA TETRARCHICA", "GUERRA CIVILE ROMANA_3", "DINASTIA COSTANTINIANA", "CASATA VALENTINIANO TEODOSIO", "CASATA TEODOSIO", "ULTIMI IMPERATORI"};
     JComboBox<String> mTendina = new JComboBox<>(dynasties);
     JButton button = new JButton("CREA ALBERO");
+
+
+
     public Window(){
         super("Imperatori Romani");
         setLayout(new BorderLayout());
+        setupListener();
     }
-    public void setup(){
+
+    private void setupListener(){
+        button.addActionListener(this);
+    }
+
+    public void setup() {
+
+
+
+
 
         //TODO: BISOGNA MODIFICARE IL PANEL CENTRALE PER RENDERLO PIU' MINIMAL E CARINO DA VEDERE.
         //TODO: BISOGNA INIZIARE AD IMPLEMENTARE ACTIONLISTENER :))
-        //TODO: MODIFICARE I COLORI E/O INSERIRE SFONDO PIU' CARINO
+
 
 
         // GESTIONE LABEL
-        /*
-        testoProvvisiorio.setText("DINASTIE ROMANE");
-        testoProvvisiorio.setFont(new Font("Sans Serif", Font.BOLD, 70));
-        testoProvvisiorio.setPreferredSize(new Dimension(1400,120));
-        testoProvvisiorio.setForeground(new Color(0x757474));
-
-         */
-
-        testoProvvisiorio.setPreferredSize(new Dimension(1400, 30));
-        testoProvvisiorio.setIcon(new ImageIcon("src/resources/images/sfondo2Nord.jpg"));
-        testoProvvisiorio2.setPreferredSize(new Dimension(1400, 30));
-        testoProvvisiorio2.setIcon(new ImageIcon("src/resources/images/sfondo2Sud.jpg"));
-        /*
-        testoProvvisiorio2.setText("Fere libenter homines id quod volunt credunt");
-        testoProvvisiorio2.setFont(new Font("Times New Roman", Font.BOLD, 20));
-        testoProvvisiorio2.setForeground(new Color(0x757474));
-
-         */
-        spazioBianco.setPreferredSize(new Dimension(10,400));
+        immagineSfondo.setPreferredSize(new Dimension(1400, 30));
+        immagineSfondo.setIcon(new ImageIcon("src/resources/images/sfondo2Nord.jpg"));
+        immagineSfondo2.setPreferredSize(new Dimension(1400, 30));
+        immagineSfondo2.setIcon(new ImageIcon("src/resources/images/sfondo2Sud.jpg"));
+        spazioBianco.setPreferredSize(new Dimension(10, 400));
         labLogo.setIcon(new ImageIcon("src/resources/logos/logoPROVA.png"));
         labLogo.setBorder(BorderFactory.createEtchedBorder());
         logoPrincipale.setHorizontalAlignment(JLabel.CENTER);
         logoPrincipale.setVisible(true);
         testoRoma.setBorder(BorderFactory.createEmptyBorder());
         testoRoma.setEditable(false);
+        testoTendina.setBounds(0,100,400,50);
         testoTendina.setForeground(new Color(0x000000));
-        testoTendina.setFont(new Font("Sans Serif", Font.PLAIN, 16));
-        labSx.setIcon(new ImageIcon("src/resources/images/Giuliosx.png"));
-        labDx.setIcon(new ImageIcon("src/resources/images/Augustodx.png"));
+        testoTendina.setFont(MyFont.creaFont("src/resources/fonts/Uni Sans Thin.ttf", 25f));
+        labSx.setIcon(new ImageIcon("src/resources/images/Augusto3sx.png"));
+        labDx.setIcon(new ImageIcon("src/resources/images/Augusto3dx.png"));
         labSPQR.setIcon(new ImageIcon("src/resources/logos/logo_spqr.png"));
-        mTendina.setBounds(2,2,10,10);
+        mTendina.setBackground(new Color(0xFAF7F7));
+        mTendina.setFocusable(false);
+        mTendina.setBorder(BorderFactory.createRaisedBevelBorder());
+        mTendina.setFont(new Font("Sans Serif", Font.PLAIN, 17));
+        mTendina.setBounds(0,150,600,30);
+
 
 
         // GESTIONE BUTTON
         button.setFocusable(false);
         button.setFont(new Font("Comic Sans", Font.BOLD, 15));
         button.setBorder(BorderFactory.createEtchedBorder());
-        //button.setBackground(Color.yellow);
+        button.setBackground(new Color(0x676060));
+        button.setForeground(Color.white);
+        button.setFont(MyFont.creaFont("src/resources/fonts/Coco-Gothic-Regular-trial.ttf", 15f));
+        button.setBounds(0,300,160,30);
 
 
         // GESTIONE PANEL
-
         pCentro.setLayout(new BorderLayout());
-        //pCentro2.setBackground(new Color(0xFFFFFF));
-        //pCentro2NORD.setBackground(new Color(0xFFFFFF));
         pCentro2.setLayout(new BorderLayout());
-        pCentro2.add(spazioBianco, BorderLayout.SOUTH);
-        pCentro2CENTRO.setLayout(new GridLayout(3,1, 0,30));
-        //pCentro2.setBackground(new Color(0xEFEBEB));
-        //pCentro2CENTRO.setBackground(new Color(0xFFFFFF));
+        //pCentro2.add(spazioBianco, BorderLayout.SOUTH);
+        pCentro2CENTRO.setLayout(null);
         pCentro2.add(pCentro2CENTRO, BorderLayout.CENTER);
         pCentro2.add(pCentro2NORD, BorderLayout.NORTH);
-        pCentro.add(pCentro2,BorderLayout.CENTER);
+        pCentro.add(pCentro2, BorderLayout.CENTER);
         pNord.setLayout(new GridBagLayout());
         pOvest.setLayout(new BorderLayout());
         pEst.setLayout(new BorderLayout());
-        pNord.add(testoProvvisiorio);
+        pNord.add(immagineSfondo);
         pNord.setBackground(new Color(0x282727));
-        //.setBackground(new Color(0xEFEBEB));       //Da levare o cambiare colore in base ai nostri gusti
         pCentro2CENTRO.add(testoTendina);
-        //pEst.setBackground(new Color(0xEFEBEB));
-        //pOvest.setBackground(new Color(0xEFEBEB));
         pCentro2CENTRO.add(mTendina);
         pCentro2CENTRO.add(button);
-        //pCentro2NORD.setBackground(new Color(0xEFEBEB));
         pCentro2NORD.add(labLogo);
         pOvest.add(labSx, BorderLayout.SOUTH);
         pEst.add(labDx, BorderLayout.SOUTH);
         pSud.setLayout(new GridBagLayout());
-        pSud.add(testoProvvisiorio2);
-        //pEst.setBackground(new Color(0xFFFFFF));
-        //pOvest.setBackground(new Color(0xFFFFFF));
-        //pSud.setBackground(new Color(0x282727));
+        pSud.add(immagineSfondo2);
 
 
         // GESTIONE DEL FRAME E DEL CONTAINER
@@ -137,5 +135,16 @@ public class Window extends JFrame{
     }
     public static void main(String[] args) {
         new Window().setup();
+    }
+
+
+    // GESTIONE DEGLI EVENTI
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String bottone = e.getActionCommand();
+        if(bottone.equals("CREA ALBERO"))
+        {
+            System.out.println("FORZA ROMA");
+        }
     }
 }
