606a58e7118976b23a09d212a3893fd8400adfbc
Victor Lopata <victorlopata2002@gmail.com>
2022-07-10 21:05:59 +0200

    Altri commenti per JavaDoc.

--- src/main/java/com/gui/Window/Window.java
+++ src/main/java/com/gui/Window/Window.java
@@ -15,6 +15,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Hashtable;
 
+/**
+ * Classe che rappresenta la finestra principale dopo aver avviato il programma.
+ *
+ * <p>I componenti principali implementati in questa finestra sono: JComboBox (utilizzata per scegliere quale dinastia raffigurare),
+ * JButton (per caricare i dati), JSlider (utulizzata per scegliere quante dinastie caricare) e la JProgressBar (utilizzatta come barra di caricamento)</p>
+ *
+ * @see JComboBox
+ * @see JSlider
+ * @see JProgressBar
+ * @see JButton
+ */
 public class Window extends JFrame implements ActionListener{
 
     private final JLabel mainLogo =new JLabel();
@@ -46,7 +57,9 @@ public class Window extends JFrame implements ActionListener{
     private int sliderValue = 1;
 
 
-
+    /**
+     * Costruttore della classe Window.
+     */
     public Window(){
         super("Imperatori Romani");
         setLayout(new BorderLayout());
@@ -58,12 +71,47 @@ public class Window extends JFrame implements ActionListener{
 
         progressBar = new JProgressBar(0, 100);
     }
-
+    /**
+     * In questo metodo vengono aggiunti i Listener ai vari componenti.
+     */
     private void setupListener(){
         button.addActionListener(this);
         dropdown_menu.addActionListener(this);
     }
 
+    /**
+     * setup dei vari componenti utilizzati.
+     *
+     * <p>
+     *     Gestione label: gestione dei vari oggetti di tipo JLabel. In particolare background1 e background2 vengono utilizzati per l'inserimento delle immagini sfondo2Nord.jpg e
+     *     sfondo2Sud.jpg come sfondo, labLogo utilizzata per l'inserimento del logo principale al centro dello schermo, dropdown_Text raffigura il testo principale "Scegli la dinastia"
+     *     e infine labSx e labDx che raffigurano le statue di Cesare e Augusto.
+     * </p>
+     *
+     * <p>
+     *     Gestione Combobox: sezione dedicata alla gestione di dropdown_menu.
+     * </p>
+     *
+     * <p>
+     *     Gestione Button: sezione dedicata alla gestione del buttone principale button.
+     * </p>
+     *
+     * <p>
+     *     Gestione ProgressBar: gestione della barra di caricamento
+     * </p>
+     *
+     * <p>
+     *     Gestione slider: gestione dello slider.
+     * </p>
+     *
+     * <p>
+     *     Gestione panel: gestione dei vari panel -> panelCenter, panelCenter2, pCenter2CENTER, panelNord, panelWest, panelEast, panelSud, pCenter2NORD.
+     * </p>
+     *
+     * <p>
+     *     Gestione frame: gestione del frame e dei suoi componenti.
+     * </p>
+     */
     public void setup() {
         // GESTIONE LABEL
         background1.setPreferredSize(new Dimension(1400, 30));
@@ -71,14 +119,14 @@ public class Window extends JFrame implements ActionListener{
         background2.setPreferredSize(new Dimension(1400, 30));
         background2.setIcon(new ImageIcon("src/resources/images/sfondo2Sud.jpg"));
         labLogo.setIcon(new ImageIcon("src/resources/logos/logo_small.png"));
-        mainLogo.setHorizontalAlignment(JLabel.CENTER);
-        mainLogo.setVisible(true);
         dropdown_Text.setBounds(188,100,400,50);
         dropdown_Text.setForeground(new Color(0x000000));
         dropdown_Text.setFont(MyFont.creaFont("src/resources/fonts/Uni Sans Thin.ttf", 25f));
         labSx.setIcon(new ImageIcon("src/resources/images/GC2.png"));
         labDx.setIcon(new ImageIcon("src/resources/images/Augusto3dx.png"));
         labSPQR.setIcon(new ImageIcon("src/resources/logos/logo_spqr.png"));
+
+        // GESTIONE COMBOBOX
         dropdown_menu.setBackground(new Color(0xFAF7F7));
         dropdown_menu.setFocusable(false);
         dropdown_menu.setBorder(BorderFactory.createRaisedBevelBorder());
@@ -191,7 +239,10 @@ public class Window extends JFrame implements ActionListener{
     }
 
 
-    // GESTIONE DEGLI EVENTI
+    /**
+     *  Override del metodo actionPerformed.
+     * @param e the event to be processed
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         String bottone = e.getActionCommand();
